10988df1c87525b26b15ae096dee43558e2cd733
fisummer <fisummer@gmail.com>
2017-09-07 14:03:11 +0800

    add weixin veriry script

---
+++ app/Http/Controllers/WxController.php
@@ -0,0 +1,50 @@
+<?php
+    namespace App\Http\Controllers;
+    class WxController extends Controller
+    {
+
+	    public function api()
+	    {
+		    $echostr = $_GET['echostr'];
+	        if(self::verifySignature()){
+			echo $echostr;
+			exit;
+		
+		}
+	    
+	    
+	    }
+        private static function verifySignature()
+        {
+		//define data send from weixin server 
+		$timestamp = $_GET['timestamp'];
+	        $nonce     = $_GET['nonce'];
+	        $signature = $_GET['signature'];
+	        $echostr   = $_GET['echostr'];
+	        //define the token had set
+		$token     = 'weixin';
+	        //combine the needed parameters to an array
+		$tmpArr = array($timestamp,$nonce,$token);
+	 	//sort the array
+		sort($tmpArr);
+		//convert the array to string
+		$tmpStr = implode($tmpArr); 
+		$tmpStr = sha1($tmpStr);
+		//compare the send signature with the produce one
+		if($tmpStr == $echostr){
+		    
+			return $echostr;
+			exit;
+		    
+		
+		}else{
+		    
+		    return false; 
+		
+		}	
+        
+    
+        }
+    
+    
+    }
