afdb53b4881c8c7fe362abd9de4a6833265f8667
fisummer <fisummer@gmail.com>
2017-09-08 14:46:20 +0800

    reponse for  the subscribe event

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -37,45 +37,41 @@
 		}
         }
 
-        public function api(){
+        public function api()
+        {
 
-            //the template data struct send from wechat
-         $receiveTemplate =  "<xml>
-<ToUserName><![CDATA[toUser]]></ToUserName>
-<FromUserName><![CDATA[FromUser]]></FromUserName>
-<CreateTime>123456789</CreateTime>
-<MsgType><![CDATA[event]]></MsgType>
-<Event><![CDATA[subscribe]]></Event>
-</xml>";
-
-         //the template data struct to be send
-         $sendTemplate = "<xml>
- <ToUserName><![CDATA[%s]]></ToUserName>
- <FromUserName><![CDATA[%s]]></FromUserName>
- <CreateTime>%s</CreateTime>
- <MsgType><![CDATA[%s]]></MsgType>
- <Content><![CDATA[%s]]></Content>
- </xml>";
-
-         //get the detail data
-
-            $xmlStr = file_get_contents('php://input');
-            $xmlObj = simplexml_load_string($xmlStr);
-
-            //define the data to be send
-
-            $touser = $xmlObj->FromUserName;
-            $fromuser = $xmlObj->ToUserName;
-            $time = time();
-            $msgType = 'text';
-            $content = 'thanks for your subscribtion !';
-
-           $senddata =  sprintf($sendTemplate,$touser,$fromuser,$time,$msgType,$content);
-
-           echo $senddata;
-
+            //get post data, May be due to the different environments
+            $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];//php:input
+            //写入日志  在同级目录下建立php_log.txt
+//chmod 777php_log.txt(赋权) chown wwwphp_log.txt(修改主)
+            error_log(var_export($postStr,1),3,'php_log.txt');
+//日志图片
+            //extract post data
+            if (!empty($postStr)){
+                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                   the best way is to check the validity of xml by yourself */
+                libxml_disable_entity_loader(true);
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+                           <ToUserName><![CDATA[%s]]></ToUserName>
+                           <FromUserName><![CDATA[%s]]></FromUserName>
+                           <CreateTime>%s</CreateTime>
+                           <MsgType><![CDATA[%s]]></MsgType>
+                           <Content><![CDATA[%s]]></Content>
+                           <FuncFlag>0</FuncFlag>
+                           </xml>";
+                //订阅事件
+                if($postObj->Event=="subscribe")
+                {
+                    $msgType = "text";
+                    $contentStr = "欢迎关注安子尘，微信babyanzichen";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }
 
 
-        }
-
-    }
+        }}}
